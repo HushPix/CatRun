08782cdeb0001cb79b82f16dd9ddf05aa8a42b08
HushPix <hushpix03@protonmail.com>
2025-02-20 16:41:35 +0100

    working on new canvas system

---
+++ scripts/CanvasScripts/Menu Data.gd
@@ -0,0 +1,10 @@
+class_name Menu_Data
+extends Control
+
+enum menu_state {
+	hidden,
+	shown,
+	current
+}
+
+var currentMenuState: menu_state
---
+++ scripts/managers/canvasManager.gd
@@ -0,0 +1,72 @@
+class_name CanvasManager
+extends CanvasLayer
+
+
+
+@export var player: Player
+@export var gameplay: Gameplay 
+@export var collectibleManager: CollectibleManager
+
+func _hideMenu(menu: Menu_Data) -> void:
+	if menu.currentMenuState == Menu_Data.menu_state.shown:
+		menu.hide()
+	
+
+
+#@onready var main_menu: Control = $MainMenu
+#@onready var gameplay_hud: Control = $GameplayHud
+#@onready var score_label: Label = $GameplayHud/scoreLabel
+#@onready var pause_menu: Control = $PauseMenu
+#@onready var game_over_screen: Control = $gameOverScreen
+#@onready var score_label_2: Label = $gameOverScreen/scoreLabel2
+#@onready var hi_score_label: Label = $GameplayHud/hiScoreLabel
+#@onready var hi_score_label_menu: Label = $MainMenu/hiScoreLabelMenu
+#@onready var start_button: AudioButton = $MainMenu/StartButton
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#player.playerDied.connect(on_game_over)
+	#pause_menu.hide()
+#
+#func _updateScore(label: Label, isHiScore: bool) -> void: #updates the score every frame
+	#if(isHiScore):
+		#label.text = "BEST:" + str(collectibleManager.getHighScore())
+	#else:
+		#label.text = str(collectibleManager.getScore())
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#if player._isPlayerAlive():
+		#_updateScore(score_label, false)
+#
+## Hides the main menu and shows hud, when player starts the game
+#func _on_start_button_pressed() -> void: ## When player is ready
+	#main_menu.hide()
+	#gameplay_hud.show()
+	#_updateScore(hi_score_label, true)
+	#
+#
+## This happens when the player enters the scene
+#func _on_gameplay_ready() -> void: ## When the scene starts
+	#_updateScore(hi_score_label_menu, true)
+	#main_menu.show()
+	#gameplay_hud.hide()
+	#
+#func on_game_over() -> void:
+	#gameplay_hud.hide()
+	#_updateScore(score_label_2, false)
+	#game_over_screen.show()
+#
+#
+#func _on_pause_button_pressed() -> void:
+	#pause_menu.show()
+#
+#
+#func _on_un_pause_button_pressed() -> void:
+	#pause_menu.hide()
+
+#func _unhandled_input(event: InputEvent) -> void:
+	#if event.is_action_pressed("uiButtonAccept"):
+		#if main_menu.visible:
+			#start_button.emit_signal("pressed")
