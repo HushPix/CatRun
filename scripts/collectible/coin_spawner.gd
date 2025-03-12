ff509641f33e6e663ce58a209db86e4cd387b88a
HushPix <hushpix03@protonmail.com>
2025-03-12 20:10:25 +0100

    mam dosc

--- scripts/collectible/coin_spawner.gd
+++ scripts/collectible/coin_spawner.gd
@@ -1,6 +1,8 @@
 extends Node2D
 class_name CoinSpawner
 
+@export var spawnDelayTimer: Timer
+@export var spawnDelay: int
 @export var coinParent: Node2D
 @export var collectibleTypes: Array[CollectibleData] = []
 @export var gameplay: Gameplay
@@ -9,23 +11,28 @@ class_name CoinSpawner
 
 var collectibleObject: PackedScene
 var spawnedCollectible : Collectible
-var spawnPosition: Vector2
+var maxCollectibles: int = 1
+var maxCollectiblesPerSpawn: int = 0
+var activeCollectibles: int = 0
+
+var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	spawnPosition = Vector2( $".".global_position.x, 24)
+	randomize()
+	spawnDelayTimer.wait_time = spawnDelay
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if(gameplay.getDifficulty() != gameplay.level.IDLE):
-		spawnCoin()
+		if spawnDelayTimer.timeout and activeCollectibles < maxCollectibles:
+			spawnCoin()
+		
 
 func spawnCoin() -> void:
 	collectibleObject = load(collectibleObjectPath)
 	spawnedCollectible = collectibleObject.instantiate() as Collectible
-	
 	spawnedCollectible.data = collectibleTypes[0]
 	spawnedCollectible.speed = gameplay.getSpeed()
-	spawnedCollectible.position = spawnPosition
-
 	coinParent.add_child(spawnedCollectible)
+	activeCollectibles += 1
--- scripts/collectible/collectible.gd
+++ scripts/collectible/collectible.gd
@@ -1,14 +1,16 @@
 class_name Collectible extends Node2D
 
 @export var sprite_2d: Sprite2D
-@export var collider: Area2D
+@export var playerDetector: Area2D
+@export var groundDetector: Area2D
 @export var collectible_audio: AudioStreamPlayer 
 @export var animatableBody: AnimatableBody2D
-var data: CollectibleData
+@export var data: CollectibleData
 
 var type: CollectibleType.Type
 var points: int
 var speed: float = 2
+var isInGround: bool
 
 func setCollectibleData(data: CollectibleData):
 	sprite_2d.texture = data.sprite
@@ -23,7 +25,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	print(isInGround)
+	move()
 
 func _physics_process(delta: float) -> void:
 	animatableBody.move_and_collide(Vector2(-speed, 0))
@@ -33,18 +36,21 @@ func get_type() -> CollectibleType.Type:
 	
 func get_points() -> int:
 	return points
-
-func _on_area_2d_body_entered(body: Player) -> void:
+	
+func collectCoin() -> void:
 	collectible_audio.play()
 	
 	sprite_2d.visible = false
-	collider.set_deferred("monitoring", false)
+	playerDetector.set_deferred("monitoring", false)
 	
 	SignalManager.passColectible.emit(self)
 	
 	await collectible_audio.finished
 	queue_free()
 
-func _on_area_2d_ground_enter(body: TileMapLayer) -> void:
-	position.y += 16
+func _on_area_2d_body_entered(body) -> void:
+	collectCoin()
+
+func move() -> void :
 	
+		position.y -= 1
